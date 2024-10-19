77b8a44c7ee5af3cb19a65999d295c4df4b2bd22
Nahuel Rodriguez <12597182+Nahuel92@users.noreply.github.com>
2024-10-19 19:33:19 -0300

    feat: Avoid IDE restarts on plugin unload
    Signed-off-by: Nahuel Rodriguez <12597182+Nahuel92@users.noreply.github.com>

--- src/main/java/io/github/nahuel92/pit4u/gui/Pit4USettingsEditor.java
+++ src/main/java/io/github/nahuel92/pit4u/gui/Pit4USettingsEditor.java
@@ -3,13 +3,16 @@ package io.github.nahuel92.pit4u.gui;
 import com.intellij.icons.AllIcons;
 import com.intellij.ide.util.PackageChooserDialog;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.ComponentWithBrowseButton;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import com.intellij.openapi.util.Disposer;
 import io.github.nahuel92.pit4u.configuration.Pit4URunConfiguration;
-import io.github.nahuel92.pit4u.gui.table.MyDialog;
-import io.github.nahuel92.pit4u.gui.table.MyTableItem;
+import io.github.nahuel92.pit4u.gui.table.OtherParamItem;
+import io.github.nahuel92.pit4u.gui.table.OtherParamsDialog;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -17,20 +20,75 @@ import java.awt.event.ActionListener;
 import java.util.stream.Collectors;
 
 public class Pit4USettingsEditor extends SettingsEditor<Pit4URunConfiguration> {
+    private static final Logger log = Logger.getInstance(Pit4USettingsEditor.class);
     private JPanel jPanel;
+
     private TextFieldWithBrowseButton targetClasses;
+    private final ActionListener targetClassesActionListener;
+
     private TextFieldWithBrowseButton targetTests;
+    private final ActionListener targetTestsActionListener;
+
     private TextFieldWithBrowseButton sourceDir;
+
     private TextFieldWithBrowseButton reportDir;
+    private final ActionListener otherParamsActionListener;
     private TextFieldWithBrowseButton otherParams;
+    private OtherParamsDialog otherParamsDialog;
 
     public Pit4USettingsEditor(final Project project) {
-        addListener(this.targetClasses, "Select Target Classes Package", project);
-        addListener(this.targetTests, "Select Target Tests Package", project);
-        addListener(this.sourceDir, "Select Source Directory");
-        addListener(this.reportDir, "Select Report Directory");
+        Disposer.register(project, this);
+
+        this.targetClassesActionListener = getActionListener(
+                "Select Target Classes Package",
+                project,
+                this.targetClasses
+        );
+        this.targetClasses.addActionListener(this.targetClassesActionListener);
+
+        this.targetTestsActionListener = getActionListener(
+                "Select Target Tests Package",
+                project,
+                this.targetTests
+        );
+        this.targetTests.addActionListener(this.targetTestsActionListener);
+
+        this.sourceDir.addBrowseFolderListener(
+                "Select Source Directory",
+                null,
+                project,
+                FileChooserDescriptorFactory.createSingleFolderDescriptor()
+        );
+
+        this.reportDir.addBrowseFolderListener(
+                "Select Report Directory",
+                null,
+                project,
+                FileChooserDescriptorFactory.createSingleFolderDescriptor()
+        );
+
         this.otherParams.setButtonIcon(AllIcons.FileTypes.Archive);
-        this.otherParams.addActionListener(addListener());
+        this.otherParamsActionListener = getActionListener();
+        this.otherParams.addActionListener(this.otherParamsActionListener);
+    }
+
+    @Override
+    protected void disposeEditor() {
+        log.info("Disposing Settings Editor");
+
+        this.targetClasses.removeActionListener(this.targetClassesActionListener);
+        this.targetTests.removeActionListener(this.targetTestsActionListener);
+
+        for (final var actionListener : this.sourceDir.getListeners(ComponentWithBrowseButton.BrowseFolderActionListener.class)) {
+            this.sourceDir.removeActionListener(actionListener);
+        }
+
+        for (final var actionListener : this.reportDir.getListeners(ComponentWithBrowseButton.BrowseFolderActionListener.class)) {
+            this.reportDir.removeActionListener(actionListener);
+        }
+        this.otherParams.removeActionListener(this.otherParamsActionListener);
+
+        log.info("Settings Editor disposed");
     }
 
     public String getTargetClasses() {
@@ -79,34 +137,27 @@ public class Pit4USettingsEditor extends SettingsEditor<Pit4URunConfiguration> {
     protected void applyEditorTo(@NotNull final Pit4URunConfiguration s) {
     }
 
-    private void addListener(final TextFieldWithBrowseButton field, final String title, final Project project) {
-        field.addActionListener(e -> {
+    private ActionListener getActionListener(final String title, final Project project,
+                                             final TextFieldWithBrowseButton field) {
+        return e -> {
             final var packageChooser = new PackageChooserDialog(title, project);
             if (packageChooser.showAndGet()) {
                 field.setText(packageChooser.getSelectedPackage().getQualifiedName() + ".*");
             }
-        });
+        };
     }
 
-    private void addListener(final TextFieldWithBrowseButton field, final String title) {
-        field.addBrowseFolderListener(
-                title,
-                null,
-                null,
-                FileChooserDescriptorFactory.createSingleFolderDescriptor()
-        );
-    }
-
-    private ActionListener addListener() {
+    private ActionListener getActionListener() {
         return e -> ApplicationManager.getApplication().invokeLater(
                 () -> {
-                    final var myDialog = new MyDialog();
-                    if (myDialog.showAndGet()) {
-                        final var otherParams1 = myDialog.getTableItems()
+                    var a = new OtherParamsDialog();
+
+                    if (a.showAndGet()) {
+                        final var otherParams = a.getTableItems()
                                 .stream()
-                                .map(MyTableItem::toString)
+                                .map(OtherParamItem::toString)
                                 .collect(Collectors.joining(" "));
-                        this.otherParams.setText(otherParams1);
+                        this.otherParams.setText(otherParams);
                     }
                 }
         );
